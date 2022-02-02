809dce8f5effe627d4d318083bcbe7226725d583
Gstone <76681984+Gst0ne@users.noreply.github.com>
2022-02-02 13:33:30 +0530

    Add files via upload

---
+++ pomice/__init__.py
@@ -0,0 +1,24 @@
+"""Pomice wrapper for Lavalink, made possible by cloudwithax 2021"""
+import discord
+
+if not discord.__version__.startswith("2.0"):
+    class DiscordPyOutdated(Exception):
+        pass
+
+    raise DiscordPyOutdated(
+        "You must have discord.py 2.0 or a discord.py fork that uses the 'discord' namespace "
+        "(a.k.a: you import the libary using 'import discord') to use this library. "
+        "Uninstall your current version and install discord.py 2.0 or a compatible fork."
+    )
+
+__version__ = ":D"
+__title__ = "pomice"
+__author__ = "cloudwithax"
+
+from .enums import SearchType
+from .events import *
+from .exceptions import *
+from .filters import *
+from .objects import *
+from .player import Player
+from .pool import *
