09d8aee35c02010ebb3154e8a985fddaf15e4a0a
Gstone <76681984+Gst0ne@users.noreply.github.com>
2022-02-13 15:01:33 +0530

    Update player.py

--- pomice/player.py
+++ pomice/player.py
@@ -229,7 +229,7 @@ class Player(VoiceProtocol):
         self._node._players[self.guild.id] = self
         self._is_connected = True
 
-    async def move_to(self, channel: Connectable) -> type(self):
+    async def move_to(self, channel: Connectable) -> None:
         await self.guild.change_voice_state(channel, self_mute=False, self_deaf=False)
 
     async def stop(self) -> None:
