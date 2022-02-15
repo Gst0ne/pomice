00629e4dd868815e09fa3c08ea7b3d6e0888d4e3
Gstone <76681984+Gst0ne@users.noreply.github.com>
2022-02-15 12:37:38 +0530

    Update enums.py

--- pomice/enums.py
+++ pomice/enums.py
@@ -14,10 +14,14 @@ class SearchType(Enum):
 
        SearchType.scsearch searches using SoundCloud,
        which is an alternative to YouTube or YouTube Music.
+
+       SearchType.tts adds 'speak:' prefix while searching,
+       More info here: https://github.com/DuncteBot/skybot-lavalink-plugin
     """
     ytsearch = "ytsearch"
     ytmsearch = "ytmsearch"
     scsearch = "scsearch"
+    tts = "speak"
 
     def __str__(self) -> str:
         return self.value
