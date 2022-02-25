7922de37acabe1ddbb11ac8a774e307d0c1b0a92
Gstone <76681984+Gst0ne@users.noreply.github.com>
2022-02-25 22:53:14 +0530

    Update track.py

--- pomice/spotify/track.py
+++ pomice/spotify/track.py
@@ -2,6 +2,7 @@ class Track:
     """The base class for a Spotify Track"""
 
     def __init__(self, data: dict) -> None:
+        self.data = data
         self.name = data["name"]
         self.artists = ", ".join(artist["name"] for artist in data["artists"])
         self.length = data["duration_ms"]
@@ -16,6 +17,13 @@ class Track:
             self.uri = None
         else:
             self.uri = data["external_urls"]["spotify"]
+
+    @property
+    def isrc(self):
+        try:
+            return self.data["external_ids"]["isrc"]
+        except KeyError:
+            return None
 
     def __repr__(self) -> str:
         return (
