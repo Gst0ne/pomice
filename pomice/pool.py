10bd5c63a17b2b4d9e5755cce27c3246e1e85e52
Gstone <76681984+Gst0ne@users.noreply.github.com>
2022-06-09 01:49:57 +0530

    Update pool.py

--- pomice/pool.py
+++ pomice/pool.py
@@ -115,8 +115,6 @@ class Node:
                 self._spotify_client_id, self._spotify_client_secret
             )
 
-        self._bot.add_listener(self._update_handler, "on_socket_response")
-
     def __repr__(self):
         return (
             f"<Pomice.node ws_uri={self._websocket_uri} rest_uri={self._rest_uri} "
