c0f4dc633698558b6fc509a7e8ae416c478ac002
Gstone <76681984+Gst0ne@users.noreply.github.com>
2022-07-03 15:35:52 +0530

    Update and rename artist.py to toptracks.py

---
+++ pomice/spotify/toptracks.py
@@ -0,0 +1,20 @@
+from typing import List, Dict
+from .track import Track
+
+class TopTracks:
+    """The base class for Spotify Popular Tracks"""
+
+    def __init__(self, data: dict, tracks: List[Dict[str, Any]]) -> None:
+        self.name = f"Top Tracks by {data['name']}"
+        self.artists = data["name"]
+        self.tracks = list(map(Track, tracks))
+        self.total_tracks = len(tracks)
+        self.id = data["id"]
+        self.image = data["images"][0]["url"]
+        self.uri = data["external_urls"]["spotify"]
+
+    def __repr__(self) -> str:
+        return (
+            f"<Pomice.spotify.TopTracks name={self.name} artist={self.artists} id={self.id} "
+            f"total_tracks={self.total_tracks} tracks={self.tracks}>"
+        )
