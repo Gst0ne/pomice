5bd8a53e8fc770032addead855709da8e667e7af
Gstone <76681984+Gst0ne@users.noreply.github.com>
2022-07-03 22:42:37 +0530

    Update toptracks.py

--- pomice/spotify/toptracks.py
+++ pomice/spotify/toptracks.py
@@ -5,14 +5,13 @@ class TopTracks:
     """The base class for Spotify Popular Tracks"""
 
     def __init__(self, data: dict, tracks: List[Dict[str, Any]]) -> None:
-        self.data = data
         self.name = f"Top Tracks by {data['name']}"
         self.artists = data["name"]
         self.tracks = list(map(Track, tracks))
         self.total_tracks = len(tracks)
         self.id = data["id"]
-#         self.image = data["images"][0]["url"]
-#         self.uri = data["external_urls"]["spotify"]
+        self.image = data["images"][0]["url"]
+        self.uri = data["external_urls"]["spotify"]
 
     def __repr__(self) -> str:
         return (
